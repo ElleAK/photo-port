f2adde05afc55e6f669b06354770d1b83ccced5a
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-09 19:22:54 -0400

    tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,22 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-
 afterEach(cleanup);
 
-describe('About component', () => {
-    //first test
-    it('renders', () => {
-        render(<About/>);   
-    });
-      //second test
-    it('matches snapshot DOM node structure', () => {
-        //render About
-        const { asFragment } = render(<About/>);
-        expect(asFragment()).toMatchSnapshot();
-    });
-
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
-
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,36 +1,47 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
-//clean up
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
-describe("Nav component", () => {
-    //baseline test
-    it('renders', () => {
-        render(<Nav/>);
-    });
+describe('Nav component', () => {
+  it('renders', () => {
+    render(<Nav 
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        />);
+  });
 
-    //snapshot test
-    it('matches snapshot', () => {
-        const { asFragment } = render(<Nav/>);
-        // assert value comparison
-        expect(asFragment()).toMatchSnapshot();
-    });
-});
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
 
 describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-        const { getByLabelText } = render(<Nav/>);
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-});
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})
 
 describe('links are visible', () => {
-    it('inserts text into the links', () => {
-      const { getByTestId } = render(<Nav />);
-      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-      expect(getByTestId('about')).toHaveTextContent('About me');
-    });
-  })
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
+})
