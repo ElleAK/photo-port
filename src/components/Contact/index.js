875ac47a96b4721883da595a847e02e94780a5e0
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-09 20:40:50 -0400

    submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {useState } from 'react';
+
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
           <h1>Contact me</h1>
-          <form id="contact-form">
-            // name input
+          <form id="contact-form" onSubmit={handleSubmit}>
             <div>
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name" />
+            <input type="text" defaultValue={name} onChange={handleChange} name="name" />
             </div>
-            // email input
             <div>
             <label htmlFor="email">Email address:</label>
-            <input type="email" name="email" />
+            <input type="email" defaultValue={email} name="email" onChange={handleChange}  />
             </div>
-            // message text area
             <div>
             <label htmlFor="message">Message:</label>
-            <textarea name="message" rows="5"  />
+            <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
             </div>
             <button type="submit">Submit</button>
           </form>
