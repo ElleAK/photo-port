d774bbb244d2ec9763b9d544affb01e52ccd455a
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-09 21:18:40 -0400

    errorMeassage

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,33 @@
 import React, {useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
 
     function handleChange(e) {
-        setFormState({ ...formState, [e.target.name]: e.target.value })
-    }
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                }
+              }
+        }
+        if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    };
+
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
